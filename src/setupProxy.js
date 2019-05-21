f515aa5e64ff8b9b5c83952dd72bc25fa50b8a4d
Denis Bezrukov <anthrax63@gmail.com>
2019-05-21 22:57:47 +0300

    change config

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,10 @@
 const proxy = require('http-proxy-middleware');
 
+const apiEndpoint = process.env.API_ENDPOINT || 'https://api.jethunter.net/';
+
 
 module.exports = function(app) {
-  app.use(proxy('/graphql', {target: 'http://localhost:8080/'}));
-  app.use(proxy('/pages', {target: 'http://localhost:8080/'}));
-  app.use(proxy('/templatedata', {target: 'http://localhost:8080/'}));
+  app.use(proxy('/graphql', {target: apiEndpoint + '/graphql', changeOrigin: true}));
+  app.use(proxy('/pages', {target: apiEndpoint, changeOrigin: true}));
+  app.use(proxy('/templatedata', {target: apiEndpoint, changeOrigin: true}));
 };
